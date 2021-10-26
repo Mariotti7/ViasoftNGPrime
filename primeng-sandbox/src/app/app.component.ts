79f198ee0a58d77823fcf8ada4ea42bbd70354a6
Leandro <midiasmart99@gmail.com>
2021-10-25 23:18:36 -0300

    First look app component, service and model file

--- primeng-sandbox/src/app/app.component.ts
+++ primeng-sandbox/src/app/app.component.ts
@@ -3,8 +3,36 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'primeng-sandbox';
+
+  items = [
+    { vin: 'r3278r2', year: 2010, brand: 'Audi', color: 'Black' },
+    { vin: 'jhto2g2', year: 2015, brand: 'BMW', color: 'White' },
+    { vin: 'h453w54', year: 2012, brand: 'Honda', color: 'Blue' },
+  ];
+
+  data: any;
+
+    constructor() {
+        this.data = {
+            labels: ['A','B','C'],
+            datasets: [
+                {
+                    data: [300, 50, 100],
+                    backgroundColor: [
+                        "#FF6384",
+                        "#36A2EB",
+                        "#FFCE56"
+                    ],
+                    hoverBackgroundColor: [
+                        "#FF6384",
+                        "#36A2EB",
+                        "#FFCE56"
+                    ]
+                }]
+            };
+    }
 }
--- primeng-sandbox/src/app/app.module.ts
+++ primeng-sandbox/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ButtonModule} from 'primeng/button';
+import {TabViewModule} from 'primeng/tabview';
+import {CarouselModule} from 'primeng/carousel';
+import {ContextMenuModule} from 'primeng/contextmenu';
+import {ChartModule} from 'primeng/chart';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ButtonModule,
+    AppRoutingModule,
+    TabViewModule,
+    CarouselModule,
+    ContextMenuModule,
+    ChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
